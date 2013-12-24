df6a9459cb7cbef2ebdcb57c277b785e095776bd
Juliusz Gonera <juliusz.gonera@gmail.com>
2013-12-24 16:02:21 +0100

    Add first simple complexity testing code

--- src/app/App.scala
+++ src/app/App.scala
@@ -4,6 +4,7 @@ import graphs._
 import graphs.Graph._
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
+import java.lang.Runtime
 
 /**
  * @author Marek Lewandowski <marek.m.lewandowski@gmail.com>
@@ -31,6 +32,37 @@ object App extends scala.App {
     dimacsGraph
   }
 
+  def measureTimeAndMemory[R](codeBlock: () => R): (R, Long, Long) = {
+    val start = System.currentTimeMillis()
+    val startMemory = Runtime.getRuntime().freeMemory
+
+    val result = codeBlock()
+
+    val endMemory = Runtime.getRuntime().freeMemory
+    val time = System.currentTimeMillis() - start
+    val kb = (startMemory - endMemory)/1024
+    (result, time, kb)
+  }
+
+  def measureTimeAndMemoryComplexity() = {
+    for (n <- 10 to 40) {
+      val sampleSize = 4
+      var durationSum: Long = 0
+      var memorySum: Long = 0
+      for (j <- 1 to sampleSize) {
+        val g = Graph.randomUndirectedGraph(n, 0.8)
+        val (result, duration, memory) = measureTimeAndMemory( () => Graph.bronKerbosch(g))
+        durationSum += duration
+        memorySum += memory
+      }
+
+      val avgDuration: Double = durationSum/sampleSize
+      val avgMemory: Double = memorySum/sampleSize
+
+      println(s"${n} ${avgDuration} ${avgMemory}")
+    }
+  }
+
 
   override def main(args: Array[String]): Unit = {
     println("Begin reading stdin")
@@ -44,7 +76,6 @@ object App extends scala.App {
     val start: Duration = Duration(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
     val clique: Set[Node] = Graph.maximalClique(g)
     val time: Duration = Duration(System.currentTimeMillis(), TimeUnit.MILLISECONDS) - start
-
     println(s"graph w(g) TIME\n${dimacsGraph.name} ${clique.size} ${time.toSeconds}")
   }
 }
